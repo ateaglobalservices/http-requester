35c7e2cbb39c2ae54fa265fbdcab1f470ab0dcec
Kristoffer Hägelstam <kristoffer.hagelstam@outlook.com>
2020-05-27 19:25:18 +0200

    switched to hyper and added support for multiple headers

--- src/config.rs
+++ src/config.rs
@@ -1,32 +1,24 @@
 use std::str::FromStr;
 use std::error::Error;
 
+use serde::{Deserialize, Serialize};
+use serde_json::{Value, Map};
+
+#[derive(Serialize, Deserialize)]
 pub struct Config {
+    pub requests_to_send: u64,
     pub method: String,
-    pub amount: u64,
     pub uri: String,
-    pub authorization: String,
-    pub body: serde_json::Value,
+    pub headers: Map<String, Value>,
+    pub body: Value,
 }
 
 impl FromStr for Config {
     type Err = Box<dyn Error>;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let json: serde_json::Value = serde_json::from_str(&s)?;
+        let config: Config = serde_json::from_str(&s)?;
 
-        let method = json.get("method").unwrap().as_str().expect("no method specified");
-        let amount = json.get("amount").unwrap().as_u64().expect("no amount specified");
-        let uri = json.get("uri").unwrap().as_str().expect("no uri specified");
-        let authorization = json.get("authorization").unwrap().as_str().expect("no authorization specified");
-        let body = json.get("body").expect("no body specified");
-
-        Ok(Config {
-            method: method.to_owned(),
-            amount: amount,
-            uri: uri.to_owned(),
-            authorization: authorization.to_owned(),
-            body: body.to_owned(),
-        })
+        Ok(config)
     }
 } 
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,54 @@
 mod config;
+use config::Config;
+
+use hyper::{Client, Request, Body};
+use hyper::http::request::Builder;
+use hyper::client::HttpConnector;
+use hyper_tls::HttpsConnector;
 
 use std::time::Instant;
 use std::error::Error;
 
-use config::Config;
+use serde_json::{Value, Map};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let config: Config = std::fs::read_to_string("./config.json")?.parse()?;
 
-    for request_number in 1..config.amount {
-        send_request(request_number, &config).await?;
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, Body>(https);
+
+    for request_number in 0..config.requests_to_send {
+        let body = match config.body.as_str() {
+            Some(body) => body,
+            None => "",
+        };
+
+        send_request(&client, request_number, &config.method, &config.uri, &config.headers, body.to_owned()).await?;
     }
 
     Ok(())
 }
 
-async fn send_request(request_number: u64, config: &Config) -> Result<(), Box<dyn Error>> {
+fn build_request(method: &str, uri: &str, headers: &Map<String, Value>) -> Builder {
+    let mut builder = Request::builder()
+        .method(method)
+        .uri(uri);
+
+    for (key, value) in headers {
+        let value = serde_json::to_string(value).expect("failed to read header value as string");
+        builder = builder.header(key.as_str(), value);
+    }
+
+    builder
+}
+
+async fn send_request(client: &Client<HttpsConnector<HttpConnector>>, request_number: u64, method: &str, uri: &str, headers: &Map<String, Value>, body: String) -> Result<(), Box<dyn Error>> {
     let duration = Instant::now();
 
-    let request = get_request(config).unwrap();
-    let response = request.send().await?;
+    let builder = build_request(method, uri, headers);
+    let request = builder.body(Body::from(body))?;
+    let response = client.request(request).await?;
 
     let elapsed = duration.elapsed();
 
@@ -29,21 +57,4 @@ async fn send_request(request_number: u64, config: &Config) -> Result<(), Box<dy
     println!("{}: {:#?} {}ms", request_number, status, elapsed.as_millis());
     
     Ok(())
-}
-
-fn get_request(config: &Config) -> Option<reqwest::RequestBuilder> {
-    if config.method == "GET" {
-        return Some(reqwest::Client::new()
-            .get(&config.uri)
-            .header("Authorization", &config.authorization));
-    }
-    
-    if config.method == "POST" {
-        return Some(reqwest::Client::new()
-            .post(&config.uri)
-            .header("Authorization", &config.authorization)
-            .json(&config.body));
-    }
-
-    None
 }
